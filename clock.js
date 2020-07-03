b66cf76a5ac00fed364f25fae4a39c1699c0ce6b
KrithikVallem <47306032+KrithikVallem@users.noreply.github.com>
2020-07-03 15:11:57 -0400

    changed code to use prototype modification
    I wanted to try out prototype modification

--- clock.js
+++ clock.js
@@ -1,41 +1,42 @@
 function getNewTime() {
     const d = new Date();
-    const h = addPaddingZero(d.getHours());
-    const m = addPaddingZero(d.getMinutes());
-    const s = addPaddingZero(d.getSeconds());
+    const h24 = d.getHours().addPaddingZero();
+    const h12 = d.getHours().convertHoursTo12().addPaddingZero();
+    const m = d.getMinutes().addPaddingZero();
+    const s = d.getSeconds().addPaddingZero();
+    const amPm = (d.getHours() < 12) ? "AM" : "PM";
 
-    const newTime = `${h}:${m}:${s}`;
+    document.querySelector("#time24").innerText = `${h24}:${m}:${s}`;
+    document.querySelector("#time12").innerText = `${h12}:${m}:${s} ${amPm}`;
 
-    document.querySelector("#time24").innerText = newTime;
-    document.querySelector("#time12").innerText = convertTo12(h,m,s);
+    // make everything appear all at once instead of showing up separately
+    document.body.style.visibility = "visible";
 }
 
 /* turns 1:5:7 into 01:05:07 */
-function addPaddingZero(num) {
-    if (num < 10) {
-        return "0" + num;
+Number.prototype.addPaddingZero = function() {
+    if (this.valueOf() < 10) {
+        return "0" + this.valueOf();
     }
     else {
-        return num;
+        // converting to string so the function returns a string every time
+        return this.valueOf().toString(10);
     }
-}
+};
 
-function convertTo12(h,m,s) {
-    let suffix = "AM";
-
-    h = parseInt(h, 10); // make sure that h is a number not a string
-    
+Number.prototype.convertHoursTo12 = function() {
     // hour 0 is 12:00 AM
-    if (h === 0) {
-        h = 12;
+    if (this.valueOf() === 0) {
+        return 12;
     }
-    // times like 13:00 are actually 1:00 PM in 12 hour format
-    else if (h > 12) {
-        h -= 12;
-        suffix = "PM";
+    // PM times like 13:00 are 1:00 PM
+    else if (this.valueOf() > 12) {
+        return this.valueOf() - 12;
     }
+    // all AM times other than 12:00 AM
+    else {
+        return this.valueOf();
+    }
+};
 
-    return `${h}:${m}:${s} ${suffix}`;
-}
-
-setInterval(getNewTime, 1000);
+setInterval(getNewTime, 1*1000);
