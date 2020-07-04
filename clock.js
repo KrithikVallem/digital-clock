6e6146cacee15d37b064331605ff6c409eb12afd
KrithikVallem <47306032+KrithikVallem@users.noreply.github.com>
2020-07-04 00:34:57 -0400

    changed prototype names to ensure they work in future

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 function getNewTime() {
     const d = new Date();
-    const h24 = d.getHours().addPaddingZero();
-    const h12 = d.getHours().convertHoursTo12();
-    const m = d.getMinutes().addPaddingZero();
-    const s = d.getSeconds().addPaddingZero();
+    const h24 = d.getHours().kv_addPaddingZero();
+    const h12 = d.getHours().kv_convertHoursTo12();
+    const m = d.getMinutes().kv_addPaddingZero();
+    const s = d.getSeconds().kv_addPaddingZero();
     const amPm = (d.getHours() < 12) ? "AM" : "PM";
 
     document.querySelector("#time24").innerText = `${h24}:${m}:${s}`;
@@ -14,7 +14,7 @@ function getNewTime() {
 }
 
 /* turns 1:5:7 into 01:05:07 */
-Number.prototype.addPaddingZero = function() {
+Number.prototype.kv_addPaddingZero = function() {
     if (this.valueOf() < 10) {
         return "0" + this.valueOf();
     }
@@ -24,7 +24,7 @@ Number.prototype.addPaddingZero = function() {
     }
 };
 
-Number.prototype.convertHoursTo12 = function() {
+Number.prototype.kv_convertHoursTo12 = function() {
     // hour 0 is 12:00 AM
     if (this.valueOf() === 0) {
         return 12;
